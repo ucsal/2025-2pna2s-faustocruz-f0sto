08a4be9efcf874e75ccf9289eacf56f2b4177878
FAUSTO MOTA SANTOS ALVES DA CRUZ <200032557@ucsal.br>
2025-09-09 21:03:00 -0300

    começando a implementar o solid nesse projeto. Ainda faltaria testar e implementar a parte de taxa

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,10 +1,24 @@
 package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
+	
 	public String generate(Order order) {
-		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
+		double subtotal = generateSubTotal(order);
+		double tax = generateTax(subtotal, 0.10); //Taxa 10 fixa :(
 		double total = subtotal + tax;
+		
+		return ReceiptFormatter(order, subtotal, tax, total).toString();
+	}
+	
+	public double generateSubTotal(Order order) {
+		return order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
+	}
+	
+	public double generateTax(double subtotal, double tax) {
+		return subtotal * tax;
+	}
+	
+	public StringBuilder ReceiptFormatter(Order order, double subtotal, double tax, double total) {
 		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
 		sb.append("=== RECIBO ===\n");
 		for (var i : order.getItems()) {
@@ -14,6 +28,7 @@ public class ReceiptService {
 		sb.append("Subtotal: ").append(subtotal).append("\n");
 		sb.append("Tax: ").append(tax).append("\n");
 		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
+		return sb;
 	}
+	
 }
